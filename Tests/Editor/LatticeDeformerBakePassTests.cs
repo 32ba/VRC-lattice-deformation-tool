f423ca973d791f49bdf0222cc6c69e5c232ac4e5
Yuki Maruyama <dev@32ba.net>
2026-05-24 20:51:09 +0900

    Cover inspector disabled deformer bake

--- Tests/Editor/LatticeDeformerBakePassTests.cs
+++ Tests/Editor/LatticeDeformerBakePassTests.cs
@@ -1,6 +1,7 @@
 #if UNITY_EDITOR
 using Net._32Ba.LatticeDeformationTool.Editor;
 using NUnit.Framework;
+using UnityEditor;
 using UnityEngine;
 
 namespace Net._32Ba.LatticeDeformationTool.Tests.Editor
@@ -16,6 +17,26 @@ namespace Net._32Ba.LatticeDeformationTool.Tests.Editor
                 var deformer = go.AddComponent<LatticeDeformer>();
                 deformer.enabled = false;
 
+                Assert.That(LatticeDeformerBakePass.ShouldProcessDeformer(deformer), Is.False);
+            }
+            finally
+            {
+                Object.DestroyImmediate(go);
+            }
+        }
+
+        [Test]
+        public void ShouldProcessDeformer_WhenInspectorEnabledCheckboxIsUnchecked_ReturnsFalse()
+        {
+            var go = new GameObject("InspectorDisabledComponentDeformer");
+            try
+            {
+                var deformer = go.AddComponent<LatticeDeformer>();
+                var serializedDeformer = new SerializedObject(deformer);
+                serializedDeformer.FindProperty("m_Enabled").boolValue = false;
+                serializedDeformer.ApplyModifiedPropertiesWithoutUndo();
+
+                Assert.That(deformer.enabled, Is.False);
                 Assert.That(LatticeDeformerBakePass.ShouldProcessDeformer(deformer), Is.False);
             }
             finally
